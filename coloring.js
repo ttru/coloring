07ceb89a5ad4c4289837dccca83d081b2f33d828
ttru <truongtommy06@gmail.com>
2016-08-05 15:29:07 -0700

    Ring prototype

--- coloring.js
+++ coloring.js
@@ -1,10 +1,97 @@
+var Ring = function(canvas, numLayers) {
+  this.canvas = canvas;
+  this.numLayers = numLayers;
+  this.centerX = canvas.width / 2;
+  this.centerY = canvas.height / 2;
+  this.unitLength = Math.min(canvas.width, canvas.height) / (2 * numLayers + 2);
+  this.symmetry = 6;
+};
+
+Ring.prototype.setContextParams = function() {
+  if (this.canvas.getContext) {
+    var ctx = this.canvas.getContext('2d');
+    ctx.lineWidth = 2;
+    ctx.globalAlpha = 1;
+    ctx.strokeStyle = "#000000";
+    ctx.fillStyle = "#ffffff";
+    return true;
+  }
+  return false;
+};
+
+Ring.prototype.draw = function() {
+  for (var i = 0; i <= this.numLayers; i++) {
+    this.drawCircleLayer(i);
+  }
+};
+
+Ring.prototype.drawCircleLayer = function(layer) {
+  if (this.setContextParams()) {
+    var numCircles = layer * this.symmetry
+    var angleInterval = 2 * Math.PI / numCircles;
+    var startAngle = (layer % 2) * angleInterval / 2;
+    var layerRadius = layer * this.unitLength;
+    var circleRadius = this.unitLength * 0.4;
+    var ctx = this.canvas.getContext('2d');
+    for (var i = 0; i < numCircles; i++) {
+      var angle = startAngle + i * angleInterval;
+      var x = Math.cos(angle) * layerRadius + this.centerX;
+      var y = Math.sin(angle) * layerRadius + this.centerY;
+      ctx.beginPath();
+      ctx.arc(x, y, circleRadius, 0, 2 * Math.PI);
+      ctx.closePath();
+      ctx.stroke();
+    }
+  }
+};
+
+var testHappy = function() {
+  var canvas = $("#coloring-page")[0];
+  if (canvas.getContext) {
+    var ctx = canvas.getContext('2d');
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.lineWidth = 3;
+    ctx.globalAlpha = 1;
+    ctx.strokeStyle = "#000000";
+    ctx.fillStyle = "#000000";
+    ctx.beginPath();
+    ctx.arc(75,75,50,0,Math.PI*2,true); // Outer circle
+    ctx.moveTo(110,75);
+    ctx.arc(75,75,35,0,Math.PI,false);  // Mouth (clockwise)
+    ctx.moveTo(65,65);
+    ctx.arc(60,65,5,0,Math.PI*2,true);  // Left eye
+    ctx.moveTo(95,65);
+    ctx.arc(90,65,5,0,Math.PI*2,true);  // Right eye
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.arc(675,875,50,0,Math.PI*2,true); // Outer circle
+    ctx.moveTo(710,875);
+    ctx.arc(675,875,35,0,Math.PI,false);  // Mouth (clockwise)
+    ctx.moveTo(665,865);
+    ctx.arc(660,865,5,0,Math.PI*2,true);  // Left eye
+    ctx.moveTo(695,865);
+    ctx.arc(690,865,5,0,Math.PI*2,true);  // Right eye
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(0,0);
+    ctx.lineTo(canvas.width,0);
+    ctx.lineTo(canvas.width,canvas.height);
+    ctx.lineTo(0,canvas.height);
+    ctx.lineTo(0,0);
+    ctx.stroke();
+  }
+};
+
+
 $(document).ready(function() {
-  var colors = ["#C91111", "#D84E09", "#FF8000", "#F6EB20", "#51C201", "#1C8E0D", "#09C5F4", "#2862B9", "#7E44BC", "#FCA8CC", "#943F07", "#000000"];
+  var colors = ["#C91111", "#D84E09", "#FF8000", "#F6EB20", "#51C201", "#1C8E0D", "#09C5F4", "#2862B9", "#7E44BC", "#FCA8CC", "#943F07", "#000000", "#FFFFFF"];
 
   var penInfo = {
-    radius: 1,
+    radius: 2,
     color: "#C91111",
-    alpha: 0.5,
+    alpha: 0.667,
     lastX: 0,
     lastY: 0,
     active: false
@@ -15,49 +102,38 @@ $(document).ready(function() {
     if (canvas.getContext) {
       var ctx = canvas.getContext('2d');
       ctx.clearRect(0, 0, canvas.width, canvas.height);
-      ctx.lineWidth = 3;
-      ctx.globalAlpha = 1;
-      ctx.strokeStyle = "#000000";
-      ctx.fillStyle = "#000000";
-      ctx.beginPath();
-      ctx.arc(75,75,50,0,Math.PI*2,true); // Outer circle
-      ctx.moveTo(110,75);
-      ctx.arc(75,75,35,0,Math.PI,false);  // Mouth (clockwise)
-      ctx.moveTo(65,65);
-      ctx.arc(60,65,5,0,Math.PI*2,true);  // Left eye
-      ctx.moveTo(95,65);
-      ctx.arc(90,65,5,0,Math.PI*2,true);  // Right eye
-      ctx.stroke();
-
-      ctx.beginPath();
-      ctx.arc(675,875,50,0,Math.PI*2,true); // Outer circle
-      ctx.moveTo(710,875);
-      ctx.arc(675,875,35,0,Math.PI,false);  // Mouth (clockwise)
-      ctx.moveTo(665,865);
-      ctx.arc(660,865,5,0,Math.PI*2,true);  // Left eye
-      ctx.moveTo(695,865);
-      ctx.arc(690,865,5,0,Math.PI*2,true);  // Right eye
-      ctx.stroke();
-
-      ctx.beginPath();
-      ctx.moveTo(0,0);
-      ctx.lineTo(canvas.width,0);
-      ctx.lineTo(canvas.width,canvas.height);
-      ctx.lineTo(0,canvas.height);
-      ctx.lineTo(0,0);
-      ctx.stroke();
     }
+    canvas = $("#coloring-layer")[0];
+    if (canvas.getContext) {
+      var ctx = canvas.getContext('2d');
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+    var selectedPattern = $($(".pattern.selected")[0]).html();
+    switch (selectedPattern) {
+      case "Rings":
+        var ring = new Ring($("#coloring-page")[0], 10);
+        ring.draw();
+        break;
+      default:
+        testHappy();
+    }
+
   });
   $("#clear-btn").click(function() {
-    var canvas = $("#coloring-page")[0];
+    var canvas = $("#coloring-layer")[0];
     if (canvas.getContext) {
       var ctx = canvas.getContext('2d');
       ctx.clearRect(0, 0, canvas.width, canvas.height);
     }
   });
   $("#print-btn").click(function() {
-    var canvas = $("#coloring-page")[0];
-    var img = canvas.toDataURL("image/png", "file");
+    var combinedCanvas = $("#combined-canvas")[0];
+    var pageCanvas = $("#coloring-page")[0];
+    var coloringCanvas = $("#coloring-layer")[0];
+    var ctx = combinedCanvas.getContext('2d');
+    ctx.drawImage(coloringCanvas, 0, 0);
+    ctx.drawImage(pageCanvas, 0, 0);
+    var img = combinedCanvas.toDataURL("image/png", "file");
     var win = window.open(img);
     if (win) {
       //Browser has allowed it to be opened
@@ -69,8 +145,13 @@ $(document).ready(function() {
     }
   });
   $("#export-btn").click(function() {
-    var canvas = $("#coloring-page")[0];
-    var img = canvas.toDataURL("image/png", "file");
+    var combinedCanvas = $("#combined-canvas")[0];
+    var pageCanvas = $("#coloring-page")[0];
+    var coloringCanvas = $("#coloring-layer")[0];
+    var ctx = combinedCanvas.getContext('2d');
+    ctx.drawImage(coloringCanvas, 0, 0);
+    ctx.drawImage(pageCanvas, 0, 0);
+    var img = combinedCanvas.toDataURL("image/png", "file");
     var win = window.open(img);
     if (win) {
       //Browser has allowed it to be opened
